9a23c2b9cfeb91de9f3774d9ff28c5e4d105db53
Danny Le <dannylewastaken@protonmail.com>
2023-06-25 00:10:22 -0400

    Get basic example working

---
+++ src/app.rs
@@ -0,0 +1,258 @@
+use anyhow::Result;
+use phobos::prelude::*;
+
+pub fn staged_buffer_upload<T: Copy>(
+	mut ctx: Context,
+	data: &[T],
+	usage: phobos::vk::BufferUsageFlags,
+) -> Result<phobos::Buffer> {
+	let staging = phobos::Buffer::new(
+		ctx.device.clone(),
+		&mut ctx.allocator,
+		data.len() as u64 * std::mem::size_of::<T>() as u64,
+		phobos::vk::BufferUsageFlags::TRANSFER_SRC,
+		phobos::MemoryType::CpuToGpu,
+	)?;
+
+	let mut staging_view = staging.view_full();
+	staging_view.mapped_slice()?.copy_from_slice(data);
+
+	let buffer = phobos::Buffer::new_device_local(
+		ctx.device,
+		&mut ctx.allocator,
+		staging.size(),
+		phobos::vk::BufferUsageFlags::TRANSFER_DST | usage,
+	)?;
+	let view = buffer.view_full();
+
+	let cmd = ctx
+		.execution_manager
+		.on_domain::<phobos::domain::Transfer>()?
+		.copy_buffer(&staging_view, &view)?
+		.finish()?;
+
+	ctx.execution_manager.submit(cmd)?.wait()?;
+	Ok(buffer)
+}
+
+#[derive(Debug)]
+pub struct WindowContext {
+	pub event_loop: winit::event_loop::EventLoop<()>,
+	pub window: winit::window::Window,
+}
+
+impl WindowContext {
+	pub fn new(title: impl Into<String>) -> Result<Self> {
+		Self::with_size(title, 800.0, 600.0)
+	}
+
+	pub fn with_size(title: impl Into<String>, width: f32, height: f32) -> Result<Self> {
+		let event_loop = winit::event_loop::EventLoopBuilder::new().build();
+		let window = winit::window::WindowBuilder::new()
+			.with_title(title)
+			.with_inner_size(winit::dpi::LogicalSize::new(width, height))
+			.build(&event_loop)?;
+		Ok(Self {
+			event_loop,
+			window,
+		})
+	}
+}
+
+pub struct VulkanContext {
+	pub frame: Option<phobos::FrameManager>,
+	pub pool: phobos::pool::ResourcePool,
+	pub execution_manager: phobos::ExecutionManager,
+	pub allocator: phobos::DefaultAllocator,
+	pub device: phobos::Device,
+	pub physical_device: phobos::PhysicalDevice,
+	pub surface: Option<phobos::Surface>,
+	pub debug_messenger: phobos::DebugMessenger,
+	pub instance: phobos::Instance,
+}
+
+#[derive(Clone)]
+pub struct Context {
+	pub device: phobos::Device,
+	pub execution_manager: phobos::ExecutionManager,
+	pub allocator: phobos::DefaultAllocator,
+	pub resource_pool: phobos::pool::ResourcePool,
+}
+
+pub trait App {
+	fn new(ctx: Context) -> Result<Self>
+	where
+		Self: Sized;
+
+	fn frame(&mut self, _ctx: Context, _ifc: phobos::InFlightContext) -> Result<phobos::sync::submit_batch::SubmitBatch<phobos::domain::All>> {
+		anyhow::bail!("frame() not implemented for non-headless example app");
+	}
+
+	fn run(&mut self, _ctx: Context) -> Result<()> {
+		Ok(())
+	}
+
+	fn handle_event(&mut self, _event: &winit::event::Event<()>) -> Result<()> {
+		Ok(())
+	}
+}
+
+pub struct Runner {
+	vk: VulkanContext,
+}
+
+impl Runner {
+	pub fn new(
+		name: impl Into<String>,
+		window: Option<&WindowContext>,
+		make_settings: impl Fn(phobos::AppBuilder<winit::window::Window>) ->
+		phobos::AppSettings<winit::window::Window>,
+	) -> Result<Self> {
+		std::env::set_var("RUST_LOG", "trace");
+		pretty_env_logger::init();
+		let mut settings = phobos::AppBuilder::new()
+			.version((1, 0, 0))
+			.name(name)
+			.validation(true)
+			.present_mode( phobos::vk::PresentModeKHR::MAILBOX)
+			.scratch_size(1 * 1024u64)
+			.gpu(phobos::GPURequirements {
+				dedicated: false,
+				min_video_memory: 1 * 1024 * 1024 * 1024, // 1 GiB
+				min_dedicated_video_memory: 1 * 1024 * 1024 * 1024,
+				queues: vec![
+					phobos::QueueRequest {
+						dedicated: false,
+						queue_type: phobos::QueueType::Graphics
+					},
+					phobos::QueueRequest {
+						dedicated: true,
+						queue_type: phobos::QueueType::Transfer,
+					},
+					phobos::QueueRequest {
+						dedicated: true,
+						queue_type: phobos::QueueType::Compute
+					}
+				],
+				..Default::default()
+			});
+
+		match window {
+			None => {}
+			Some(window) => {
+				settings = settings.window(&window.window);
+			}
+		};
+		let settings = make_settings(settings);
+
+		let (instance, physical_device, surface, device, allocator, pool,
+		exec, frame, Some(debug_messenger)) = phobos::initialize(&settings,
+		window.is_none())? else {
+			panic!("Asked for debug messenger but didn't get one")
+		};
+
+		let vk = VulkanContext {
+			frame,
+			pool,
+			execution_manager: exec,
+			allocator,
+			device,
+			physical_device,
+			surface,
+			debug_messenger,
+			instance,
+		};
+
+		Ok(Self {
+			vk,
+		})
+	}
+
+	fn run_headless<E: App + 'static>(self, mut app: E) -> ! {
+		app.run(self.make_context()).unwrap();
+		self.vk.device.wait_idle().unwrap();
+		drop(app);
+		std::process::exit(0);
+	}
+
+	fn make_context(&self) -> Context {
+		Context {
+			device: self.vk.device.clone(),
+			execution_manager: self.vk.execution_manager.clone(),
+			allocator : self.vk.allocator.clone(),
+			resource_pool: self.vk.pool.clone(),
+		}
+	}
+
+	fn frame<E: App + 'static>(&mut self, app: &mut E, window: &winit::window::Window) -> Result<()> {
+		let ctx = self.make_context();
+		let frame = self.vk.frame.as_mut().unwrap();
+		let surface = self.vk.surface.as_ref().unwrap();
+		futures::executor::block_on(
+			frame.new_frame(self.vk.execution_manager.clone(), window, surface,
+			|ifc| app.frame(ctx, ifc)),
+		)?;
+		Ok(())
+	}
+
+	fn run_windowed<E: App + 'static>(mut self, app: E, window: WindowContext) -> ! {
+		let event_loop = window.event_loop;
+		let window = window.window;
+		let mut app = Some(app);
+		event_loop.run(move |event, _, control_flow| {
+			// Do not render a frame if Exit control flow is specified, to avoid
+			// sync issues.
+			if let winit::event_loop::ControlFlow::ExitWithCode(_) = *control_flow {
+				self.vk.device.wait_idle().unwrap();
+				return;
+			}
+			*control_flow = winit::event_loop::ControlFlow::Poll;
+
+			match &mut app {
+				None => {}
+				Some(app) => {
+					app.handle_event(&event).unwrap();
+				}
+			}
+
+			// Note that we want to handle events after processing our current frame, so that
+			// requesting an exit doesn't attempt to render another frame, which causes
+			// sync issues.
+			match event {
+				winit::event::Event::WindowEvent {
+					event: winit::event::WindowEvent::CloseRequested,
+					window_id,
+				} if window_id == window.id() => {
+					*control_flow = winit::event_loop::ControlFlow::Exit;
+					self.vk.device.wait_idle().unwrap();
+					let app = app.take();
+					match app {
+						None => {}
+						Some(app) => {
+							drop(app);
+						}
+					}
+				}
+				winit::event::Event::MainEventsCleared => {
+					window.request_redraw();
+				}
+				winit::event::Event::RedrawRequested(_) => match app.as_mut() {
+					None => {}
+					Some(app) => {
+						self.frame(app, &window).unwrap();
+						self.vk.pool.next_frame();
+					}
+				},
+				_ => (),
+			}
+		})
+	}
+
+	pub fn run<E: App + 'static>(self, window: Option<WindowContext>) -> ! {
+		let app = E::new(self.make_context()).unwrap();
+		match window {
+			None => self.run_headless(app),
+			Some(window) => self.run_windowed(app, window)
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,185 @@
-fn main() {
-    println!("Hello, world!");
+use winit::window::WindowBuilder;
+use anyhow::Result;
+use phobos::image;
+use phobos::prelude::*;
+
+mod app;
+mod spirv;
+
+struct Resources {
+    pub offscreen: phobos::Image,
+    pub offscreen_view: phobos::ImageView,
+    pub sampler: phobos::Sampler,
+    pub vertex_buffer: phobos::Buffer,
+}
+
+struct Basic {
+    resources: Resources,
+}
+
+impl app::App for Basic {
+    fn new(mut ctx: app::Context) -> Result<Self>
+    where
+        Self: Sized, {
+        // Load shader
+        let vtx_code = spirv::load_spirv_file(std::path::Path::new("shaders/vert.spv"));
+        let frag_code = spirv::load_spirv_file(std::path::Path::new("shaders/frag.spv"));
+
+        let vertex = phobos::ShaderCreateInfo::from_spirv(vk::ShaderStageFlags::VERTEX, vtx_code);
+        let fragment = phobos::ShaderCreateInfo::from_spirv(vk::ShaderStageFlags::FRAGMENT, frag_code);
+
+        // Now we can start using the pipeline builder to create our full pipeline.
+        let pci = PipelineBuilder::new("sample".to_string())
+            .vertex_input(0, vk::VertexInputRate::VERTEX)
+            .vertex_attribute(0, 0, vk::Format::R32G32_SFLOAT)?
+            .vertex_attribute(0, 1, vk::Format::R32G32_SFLOAT)?
+            .dynamic_states(&[vk::DynamicState::VIEWPORT, vk::DynamicState::SCISSOR])
+            .blend_attachment_none()
+            .cull_mask(vk::CullModeFlags::NONE)
+            .attach_shader(vertex.clone())
+            .attach_shader(fragment)
+            .build();
+
+        // Store the pipeline in the pipeline cache
+        ctx.resource_pool.pipelines.create_named_pipeline(pci)?;
+
+        let frag_code = spirv::load_spirv_file(std::path::Path::new("shaders/blue.spv"));
+        let fragment = phobos::ShaderCreateInfo::from_spirv(vk::ShaderStageFlags::FRAGMENT, frag_code);
+
+        let pci = PipelineBuilder::new("offscreen".to_string())
+            .vertex_input(0, vk::VertexInputRate::VERTEX)
+            .vertex_attribute(0, 0, vk::Format::R32G32_SFLOAT)?
+            .vertex_attribute(0, 1, vk::Format::R32G32_SFLOAT)?
+            .dynamic_states(&[vk::DynamicState::VIEWPORT, vk::DynamicState::SCISSOR])
+            .blend_attachment_none()
+            .cull_mask(vk::CullModeFlags::NONE)
+            .attach_shader(vertex)
+            .attach_shader(fragment)
+            .build();
+        ctx.resource_pool.pipelines.create_named_pipeline(pci)?;
+
+        // Define some resources we will use for rendering
+        let image = phobos::Image::new(
+            ctx.device.clone(),
+            &mut ctx.allocator,
+            800,
+            600,
+            vk::ImageUsageFlags::COLOR_ATTACHMENT | vk::ImageUsageFlags::SAMPLED,
+            vk::Format::R8G8B8A8_SRGB,
+            vk::SampleCountFlags::TYPE_1,
+        )?;
+        let data: Vec<f32> = vec![
+            -1.0, 1.0, 0.0, 1.0, -1.0, -1.0, 0.0, 0.0, 1.0, -1.0, 1.0, 0.0, -1.0, 1.0, 0.0, 1.0,
+            1.0, -1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0,
+        ];
+
+        let resources = Resources {
+            offscreen_view: image.view(vk::ImageAspectFlags::COLOR)?,
+            offscreen: image,
+            sampler: phobos::Sampler::default(ctx.device.clone())?,
+            vertex_buffer: app::staged_buffer_upload(
+                ctx.clone(),
+                data.as_slice(),
+                vk::BufferUsageFlags::VERTEX_BUFFER,
+            )?,
+        };
+
+        Ok(Self {
+            resources,
+        })
+    }
+
+    fn frame(&mut self, ctx: app::Context, ifc: InFlightContext) -> Result<phobos::sync::submit_batch::SubmitBatch<domain::All>> {
+        // Define a virtual resource pointing to the swapchain
+        let swap_resource = phobos::image!("swapchain");
+        let offscreen = phobos::image!("offscreen");
+
+        let vertices: Vec<f32> = vec![
+            -1.0, 1.0, 0.0, 1.0, -1.0, -1.0, 0.0, 0.0, 1.0, -1.0, 1.0, 0.0, -1.0, 1.0, 0.0, 1.0,
+            1.0, -1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0,
+        ];
+
+        // Define a render graph with one pass that clears the swapchain image
+        let graph = PassGraph::new();
+
+        let mut pool = phobos::pool::LocalPool::new(ctx.resource_pool.clone())?;
+
+        // Render pass that renders to an offscreen attachment
+        let offscreen_pass = PassBuilder::render("offscreen")
+            .color([1.0, 0.0, 0.0, 1.0])
+            .clear_color_attachment(&offscreen, ClearColor::Float([0.0, 0.0, 0.0, 0.0]))?
+            .execute_fn(|mut cmd, ifc, _bindings, _| {
+                // Our pass will render a fullscreen quad that 'clears' the screen, just so we can test pipeline creation
+                let mut buffer = ifc.allocate_scratch_vbo(
+                    (vertices.len() * std::mem::size_of::<f32>()) as vk::DeviceSize,
+                )?;
+                let slice = buffer.mapped_slice::<f32>()?;
+                slice.copy_from_slice(vertices.as_slice());
+                cmd = cmd
+                    .bind_vertex_buffer(0, &buffer)
+                    .bind_graphics_pipeline("offscreen")?
+                    .full_viewport_scissor()
+                    .draw(6, 1, 0, 0)?;
+                Ok(cmd)
+            })
+            .build();
+
+        // Render pass that samples the offscreen attachment, and possibly does some postprocessing to it
+        let sample_pass = PassBuilder::render(String::from("sample"))
+            .color([0.0, 1.0, 0.0, 1.0])
+            .clear_color_attachment(&swap_resource, ClearColor::Float([0.0, 0.0, 0.0, 0.0]))?
+            .sample_image(
+                offscreen_pass.output(&offscreen).unwrap(),
+                PipelineStage::FRAGMENT_SHADER,
+            )
+            .execute_fn(|cmd, _ifc, bindings, _| {
+                cmd.full_viewport_scissor()
+                   .bind_graphics_pipeline("sample")?
+                    .resolve_and_bind_sampled_image(
+                        0,
+                        0,
+                        &offscreen,
+                        &self.resources.sampler,
+                        bindings,
+                    )?
+                    .draw(6, 1, 0, 0)
+            })
+            .build();
+        // Add another pass to handle presentation to the screen
+        let present_pass = PassBuilder::present(
+            "present",
+            // This pass uses the output from the clear pass on the swap resource as its input
+            sample_pass.output(&swap_resource).unwrap(),
+        );
+        let mut graph = graph
+            .add_pass(offscreen_pass)?
+            .add_pass(sample_pass)?
+            .add_pass(present_pass)?
+            // Build the graph, now we can bind physical resources and use it.
+            .build()?;
+
+        let mut bindings = PhysicalResourceBindings::new();
+        bindings.bind_image("swapchain", &ifc.swapchain_image);
+        bindings.bind_image("offscreen", &self.resources.offscreen_view);
+        // create a command buffer capable of executing graphics commands
+        let cmd = ctx.execution_manager.on_domain::<phobos::domain::All>().unwrap();
+        // record render graph to this command buffer
+        let cmd = graph
+            .record(cmd, &bindings, &mut pool, None, &mut ())?
+            .finish()?;
+        let mut batch = ctx.execution_manager.start_submit_batch()?;
+        batch.submit_for_present(cmd, ifc, pool)?;
+        Ok(batch)
+    }
+}
+
+fn main() -> Result<()> {
+    let window = app::WindowContext::new(
+        "DARE"
+    )?;
+
+    app::Runner::new("DARE",
+                     Some(&window),
+        |s| s.build()
+    )?.run::<Basic>(Some(window))
 }
